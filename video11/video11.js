f183bf0455de95bd2c51bd0c6d6ea3a2bc662b2d
Huong2707 <hn27072003@gmail.com>
2024-09-16 00:31:55 +0700

    vong lap while,dowhile

--- video11/video11.js
+++
@@ -1,6 +0,0 @@
-console.log('Hello world from HTML')
-
-let arr=['MU','MC','Chelsea','Liver'];
-for(let i =0;i<arr.length;i++){
-    console.log('Check varriable i = ',arr[i])
-}
---
+++ video12/video12.js
@@ -0,0 +1,18 @@
+console.log('Hello world from HTML');
+
+let arrTop4=['ManCT','Chelsea','MU','Liverpool'];
+// for(let i=0;i<arrTop4.length;i++){
+//         console.log('Top4:',i+1,arrTop4[i])
+// }
+
+// let i=0;
+// do{
+//     console.log('Top:',i+1,arrTop4[i])
+//     i++;
+// }while(i<4)
+
+let i=0;
+while(i<arrTop4.length){
+    console.log("Top:",i+1,arrTop4[i])
+    i++;
+}

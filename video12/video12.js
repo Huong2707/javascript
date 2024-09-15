efff6b48b9c7f798fc1500480a92084afeebff2f
Huong2707 <hn27072003@gmail.com>
2024-09-16 00:42:09 +0700

    while,dowhile

--- video12/video12.js
+++ video12/video12.js
@@ -4,15 +4,15 @@ let arrTop4=['ManCT','Chelsea','MU','Liverpool'];
 // for(let i=0;i<arrTop4.length;i++){
 //         console.log('Top4:',i+1,arrTop4[i])
 // }
+//
+let i=0;
+do{
+    console.log('Top:',i+1,arrTop4[i])
+    i++;
+}while(i<4)
 
 // let i=0;
-// do{
-//     console.log('Top:',i+1,arrTop4[i])
+// while(i<arrTop4.length){
+//     console.log("Top:",i+1,arrTop4[i])
 //     i++;
-// }while(i<4)
-
-let i=0;
-while(i<arrTop4.length){
-    console.log("Top:",i+1,arrTop4[i])
-    i++;
-}
+// }
